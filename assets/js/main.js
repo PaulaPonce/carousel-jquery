40e9d0eac10d9371b66cf5b08d927ab6f6cca366
Paula Ponce <paula.poncez@usach.cl>
2017-07-06 19:46:34 -0400

    click event on Next button

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+	//Next button
+	$("#next-btn").on("click", function(event){
+		event.preventDefault();
+
+		var currentActiveImg = $(".img-shown"); //active image
+		var nextActiveImg = currentActiveImg.next(); //next image to shown
+
+		//back to the first image
+		if(nextActiveImg.length == 0){
+			nextActiveImg = $(".carousel-inner li").first();
+		}
+
+		currentActiveImg.removeClass("img-shown").addClass("img-hidden").css("z-index", -10); //back
+		nextActiveImg.addClass("img-shown").removeClass("img-hidden").css("z-index", 20); //front
+
+		$(".carousel-inner li").not([currentActiveImg, nextActiveImg]).css("z-index", 1); //reset
+	});
+});
