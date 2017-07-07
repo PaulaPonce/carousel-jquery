8594b28ec9e72ff0a8007367f77e8a3e02aa70b6
Paula Ponce <paula.poncez@usach.cl>
2017-07-06 23:18:08 -0400

    carousel bullets

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,26 @@
 $(document).ready(function(){
-	//function for Next button 
+	//Function for Next button 
 	function nextBtnClicked(){
 		var currentActiveImg = $(".img-shown"); //active image
 		var nextActiveImg = currentActiveImg.next(); //next image to shown
 
+		var currentActiveBullet = $(".active-bullet"); //active bullet
+		var nextActiveBullet = currentActiveBullet.next(); //next active bullet
+
 		//back to the first image
 		if(nextActiveImg.length == 0){
 			nextActiveImg = $(".carousel-inner li").first();
+			nextActiveBullet = $(".bullet").first();
 		}
 
+		//change images
 		currentActiveImg.removeClass("img-shown").addClass("img-hidden").css("z-index", -10); //back
 		nextActiveImg.addClass("img-shown").removeClass("img-hidden").css("z-index", 20); //front
-
 		$(".carousel-inner li").not([currentActiveImg, nextActiveImg]).css("z-index", 1); //reset
+
+		//change bullets
+		currentActiveBullet.removeClass("active-bullet");
+		nextActiveBullet.addClass("active-bullet");
 	}
 
 	//Next button
@@ -27,21 +35,28 @@ $(document).ready(function(){
 
 		var currentActiveImg = $(".img-shown"); //active image
 		var nextActiveImg = currentActiveImg.prev(); //previous image to shown
-
+		
+		var currentActiveBullet = $(".active-bullet"); //active bullet
+		var nextActiveBullet = currentActiveBullet.prev(); //previous active bullet
+		
 		//back to the last image
 		if(nextActiveImg.length == 0){
 			nextActiveImg = $(".carousel-inner li").last();
+			nextActiveBullet = $(".bullet").last();
 		}
 
+		//change images
 		currentActiveImg.removeClass("img-shown").addClass("img-hidden").css("z-index", -10); //back
 		nextActiveImg.addClass("img-shown").removeClass("img-hidden").css("z-index", 20); //front
-		
 		$(".carousel-inner li").not([currentActiveImg, nextActiveImg]).css("z-index", 1); //reset
+
+		//change bullets
+		currentActiveBullet.removeClass("active-bullet");
+		nextActiveBullet.addClass("active-bullet");
 	});
 
 	//Set the interval to be 5 seconds
 	setInterval(nextBtnClicked, 5000);
-	
 	/*
 	var time = setInterval (function(){
 		$("#carousel ul").animate({marginLeft: -720}, 1000, function(){
@@ -50,4 +65,16 @@ $(document).ready(function(){
 		})
 	}, 5000);
 	*/
+
+	//Function Bullet
+	$(".bullet").on("click", function(){
+		//var activeBullet = $(".bullet");
+		//activeBullet.addClass("active-bullet");
+		$('.carousel-indicators li').removeClass('active-bullet');
+        $(this).addClass('active-bullet');
+        
+		var currentActiveImg = $(".img-shown"); //active image
+		//currentActiveImg.removeClass("img-shown").addClass("img-hidden").css("z-index", -10); //back
+		currentActiveImg.addClass("img-shown").removeClass("img-hidden").css("z-index", 20); //front
+	})
 });
